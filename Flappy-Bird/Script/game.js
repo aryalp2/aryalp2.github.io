c08714d991bf95453b8c30dd97cb668b00c9ac18
Prashant Aryal <39881460+aryalp2@users.noreply.github.com>
2021-01-21 11:25:48 +0545

    Update game.js

--- Flappy-Bird/Script/game.js
+++ Flappy-Bird/Script/game.js
@@ -142,7 +142,63 @@ class Game {
     }
 }
 
+class Bird {
+    constructor(ctx) {
+        this.x = ctx.canvas.width / 2 - 200;
+        this.y = 200;
+        this.dy = 0.5;
+        this.img = new Image();
+        this.img.src = '../Images/bird.png';
+        this.img.style.width = '40px';
+        this.img.style.height = '30px';
+        this.img.onload = () => {
+            ctx.clearRect(0, 0, ctx.canvas.height, ctx.canvas.width);
+            ctx.drawImage(this.img, 0, 0, this.img.width, this.img.height);
+        }
+        this.ctx = ctx;
+    }
+    fallDown() {
+        this.y += this.dy;
+        this.dy = this.dy * GRAVITY;
+        
+    }
 
+    flyTop() {
+        this.img.src = '../Images/birdUp.png';
+        this.img.onload = () => {
+            this.y -= 30;
+            this.dy = 0.5;
+        }
+        
+    }
+
+    drawBird() {
+        this.ctx.drawImage(this.img, this.x, this.y, this.img.width, this.img.height);
+    }
+}
+
+class Obstacle {
+    constructor(ctx, dx) {
+        this.img = document.createElement('img');
+        this.img.src = '../Images/obstacle.png';
+        this.dx = dx;
+        this.ctx = ctx;
+        this.isCrossed = false;
+        this.img.onload = () => {
+            this.y = randomNumGenerator(-180, -10);
+            this.x = 700;
+            this.ctx.drawImage(this.img, this.x, this.y, this.img.width, this.img.height);
+        }
+    }
+
+    drawObstacle() {
+        this.ctx.drawImage(this.img, this.x, this.y, this.img.width, this.img.height);
+    }
+
+    updateFlappy() {
+        this.x -= this.dx;
+    }
+}
 
 
 
