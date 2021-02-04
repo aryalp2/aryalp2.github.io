63c8b835eda4a5325c2037adc9636f9824efbfd3
Prashant Aryal <39881460+aryalp2@users.noreply.github.com>
2021-02-04 18:59:46 +0545

    Update script.js

--- Final Project(Sorting Visualizer)/Script/script.js
+++ Final Project(Sorting Visualizer)/Script/script.js
@@ -1,21 +1,22 @@
-var inputArraySize=document.getElementById('arraySize');
+var inputArraySize = document.getElementById('arraySize');
 var arraySize = inputArraySize.value;
-var inputArrayGeneration=document.getElementById("arrayGenerate");
-var inputArraySpeed=document.getElementById("arraySpeed");
-var sortingAlgorithms=document.querySelectorAll(".class-algorithms li a");
+var inputArrayGeneration = document.getElementById("arrayGenerate");
+var inputArraySpeed = document.getElementById("arraySpeed");
+var sortingAlgorithms = document.querySelectorAll(".class-algorithms li a");
 
 var barHeights = [];
 var barProperty = [];
 var margin_size = 0.0999999999;
 var arrayContainer=document.getElementById("array-container");
 var extraBarHeight = 10;
+
 window.onload = generateArray();
 
 function generateRandomNumber(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-var count = 0;
+
 function generateArray() {
     arrayContainer.innerHTML = '';
     var scale = 85 / ( inputArraySize.max - inputArraySize.min );
@@ -49,7 +50,6 @@ for (var i = 0; i < sortingAlgorithms.length; i++){
 
 function visualizeAlgorithms(){
     disableLink();
-    this.classList.add('butt_selected');
     switch(this.innerHTML){
         
         case "Merge Sort":
@@ -86,9 +86,9 @@ function disableLink(){
     for (var i = 0; i < sortingAlgorithms.length; i++){
         sortingAlgorithms[i].removeEventListener('click', visualizeAlgorithms);
         inputArrayGeneration.removeEventListener('click', generateArray);
-        inputArraySize.disabled = true;
-        inputArraySpeed.disabled = true;
     }
+    inputArraySize.disabled = true;
+    inputArraySpeed.disabled = true;
 }
 
 
@@ -97,8 +97,8 @@ function enableLink(){
         for (var i = 0; i <sortingAlgorithms.length; i++){
             sortingAlgorithms[i].addEventListener('click', visualizeAlgorithms);
             inputArrayGeneration.addEventListener('click', generateArray)
-            inputArraySize.disabled=false;
-            inputArraySpeed.disabled=false;
         }
+        inputArraySize.disabled=false;
+        inputArraySpeed.disabled=false;
     }, cumulativeDelay = cumulativeDelay + delay_time);
 }
