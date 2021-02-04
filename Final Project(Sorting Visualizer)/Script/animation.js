6266ba6ba0cdbe79226e017e5202ccdbe8448f08
Prashant Aryal <39881460+aryalp2@users.noreply.github.com>
2021-02-04 21:15:14 +0545

    Update animation.js

--- Final Project(Sorting Visualizer)/Script/animation.js
+++ Final Project(Sorting Visualizer)/Script/animation.js
@@ -1,10 +1,5 @@
 var speed = 1000;
 class Visualization {
-    constructor(){
-        this.delay_time = 10000/(Math.floor(this.arraySize / 10) * speed);   //Decrease numerator to increase speed.
-        this.cumulativeDelay=0;//This is updated ov every div change so that visualization is visible.
-    }
-
     visualizationSpeed() {
         var array_speed = inputArraySpeed.value;
         var speed = Math.pow(10, (array_speed - 1));    
@@ -13,6 +8,6 @@ class Visualization {
 }
 
 var visual = new Visualization();
-var delay_time = 10000 / (Math.floor(this.arraySize / 10) * speed);
+var delay_time = 10000 / (Math.floor(arraySize / 10) * speed);
 var cumulativeDelay = 0;
 inputArraySpeed.addEventListener('input', visual.visualizationSpeed);
